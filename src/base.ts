6bbb49a952bb5d587dc7a5c8f9a8a426481d3f03
mrloldev <mrlol.yt.oficial@gmail.com>
2023-08-15 11:37:53 +0200

    feat: non-stream support

--- src/base.ts
+++ src/base.ts
@@ -1,5 +1,6 @@
 import { fetchEventSource } from "@waylaidwanderer/fetch-event-source";
 import { EventEmitter } from "events";
+import axios from "axios";
 
 export default class Base {
   apiKey: string;
@@ -21,21 +22,48 @@ export default class Base {
   }
 
   async fetch(url: string, options: any): Promise<EventEmitter> {
-    let event = new EventEmitter();
-    fetchEventSource(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + this.apiKey,
-        "x-captcha-token": this.captchaKey,
-      },
-      body: JSON.stringify(options),
-      onmessage: (msg: any) => {
-        let ev = JSON.parse(msg.data);
-        event.emit("data", ev);
-      },
-    });
-    return event;
+    let isStream = options.stream;
+    if (isStream === undefined) isStream = this.options.stream;
+    if (isStream === undefined) isStream = false;
+    let headers = {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + this.apiKey,
+      "x-captcha-token": this.captchaKey,
+    };
+    delete options.stream;
+
+    if (isStream) {
+      let event = new EventEmitter();
+      fetchEventSource(url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({
+          ...options,
+          stream: true,
+        }),
+        onmessage: (msg: any) => {
+          let ev = JSON.parse(msg.data);
+          event.emit("data", ev);
+        },
+      });
+      return event;
+    } else {
+      let res = await axios({
+        url: url,
+        method: "POST",
+        headers: {
+          ...headers,
+          // change timeout time to 3min
+          "x-timeout": "180000",
+        },
+        timeout: 180000,
+        data: JSON.stringify({
+          ...options,
+          stream: false,
+        }),
+      });
+      return res.data;
+    }
   }
   async model(data: { stream: true }): Promise<EventEmitter> {
     return await this.fetch("https://api.turing.sh/", data);
