bd4f475b3390eeac95aa102ae992835783fe84a7
mrloldev <mrlol.yt.oficial@gmail.com>
2023-08-20 16:52:39 +0200

    fix: modules export names

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import Text from "./modules/text";
-import Image from "./modules/image";
-import Audio from "./modules/audio";
-import Video from "./modules/video";
+import Text from "./modules/text.js";
+import Image from "./modules/image.js";
+import Audio from "./modules/audio.js";
+import Video from "./modules/video.js";
 
 export { Text, Image, Audio, Video };
